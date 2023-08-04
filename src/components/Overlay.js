a0a9afe9c6f62d752f83a152f640ff5c3cefb117
Romain Herault <romain@rherault.dev>
2023-08-04 15:15:29 +0200

    typo: fix typo error
    Creeated instead of created

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -15,7 +15,7 @@ export default function Overlay() {
         <h1>R3F CONFETTI COMPONENT</h1>
         <p>Adds confetti explosions to your projects with many customization options. <a href="https://gist.github.com/ektogamat/f810b36ea0d990fe480ed2d634380c8f" target="_blank">Download it here.</a></p>
         <p>
-          Creeated by Anderson Mancini.{' '}
+          Created by Anderson Mancini.{' '}
           <a href="https://market.pmnd.rs/model/korrigan-wolf" target="">
             Model credits
           </a>
