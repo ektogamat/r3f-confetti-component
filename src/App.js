a1732da2de544bc404a8ab4ea6870e1dbf8532e9
Dario Sanchez <110812521+dswhy@users.noreply.github.com>
2023-09-29 12:00:03 +0200

    feat: add forwardref, props to confetti. (#4)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, ContactShadows } from '@react-three/drei'
-import Overlay from './components/Overlay'
-import ExplosionConfetti from './components/Confetti'
-import Scene from './components/Scene'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Overlay from "./components/Overlay";
+import ExplosionConfetti from "./components/Confetti";
+import Scene from "./components/Scene";
+import { useRef } from "react";
 
 export default function App() {
+  const ref = useRef();
   return (
     <>
-      <Canvas dpr={1} shadows gl={{ antialias: false }} camera={{ position: [0, -0.3, 6], near: 0.5, fov: 45 }}>
-        <OrbitControls zoomSpeed={4} minDistance={4} maxDistance={8} maxPolarAngle={Math.PI / 2.0} />
-        <ExplosionConfetti rate={2} fallingHeight={9} amount={50} enableShadows isExploding />
+      <Canvas
+        dpr={1}
+        shadows
+        gl={{ antialias: false }}
+        camera={{ position: [0, -0.3, 6], near: 0.5, fov: 45 }}
+      >
+        <OrbitControls
+          zoomSpeed={4}
+          minDistance={4}
+          maxDistance={8}
+          maxPolarAngle={Math.PI / 2.0}
+        />
+        <ExplosionConfetti
+          ref={ref}
+          rate={2}
+          fallingHeight={9}
+          amount={50}
+          enableShadows
+          isExploding
+        />
         <Scene />
       </Canvas>
       <Overlay />
     </>
-  )
+  );
 }
